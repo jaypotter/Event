98f834888cc2d50ca0b80edcb19bf96d51a4e519
Jay Potter <j@ypotter.ca>
2024-03-31 12:50:52 -0400

    emitter

--- src/Potter/Event/Event.php
+++ src/Potter/Event/Event.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Potter\Event;
 
+use \Potter\Event\Emitter\EmitterInterface;
+
 final class Event extends AbstractEvent
 {
     use EventTrait;
     
-    public function __construct(string $id)
+    public function __construct(string $id, EmitterInterface $emitter)
     {
         $this->setId($id);
     }
